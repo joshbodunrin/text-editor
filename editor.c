4b895dda92884b74120a7e6333212cde791e4766
joshbodunrin <83253910+joshbodunrin@users.noreply.github.com>
2024-07-16 12:21:43 -0400

    put all write functions in buffer instead

--- editor.c
+++ editor.c
@@ -2,6 +2,7 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -57,33 +58,85 @@ char editorReadKey() { // reads what key is pressed
   }
   return c;
 }
+
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+  if (write(STDOUT_FILENO, "\x1b[6n",4) != 4) return -1;
+
+  while(i < sizeof(buf) - 1) {
+    if(read(STDIN_FILENO,&buf[i],1) != 1) break;
+    if(buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+  //  printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
+
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows,cols) != 2) return -1;
+
+  return 0;
+
+
+}
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
-  } else {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    return getCursorPosition(rows,cols);
+   } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
     return 0;
   }
 }
 
+/*** append buffer ***/
+
+struct abuf {
+  char  *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+  char *new = realloc(ab->b,ab->len + len); //resize buffer and point to new location (*new)
+
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len); //add new stuff in new
+  ab->b = new; 
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+  free(ab->b);
+}
 /*** output ***/
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.numRows; y++) {
-    write(STDOUT_FILENO, "~\r\n",3);
+    abAppend(ab, "~", 1);
+    if (y < E.numRows -1) {
+      abAppend(ab,"\r\n",2);
+    }
   }
 }
 
 void editorRefreshScreen() {
-  write(STDOUT_FILENO, "\x1b[2J",4); //clears out screen
-  write(STDOUT_FILENO, "\x1b[H", 3); // repositons cursor to top left
+  struct abuf ab = ABUF_INIT;
 
-  editorDrawRows();
+  abAppend(&ab, "\x1b[?25l",6); // hide cursor
+  abAppend(&ab, "\x1b[2J", 4); // clears out screen
+  abAppend(&ab, "\x1b[H",3);  // repositons cursor to top left
 
-  write(STDOUT_FILENO, "\x1b[H",3);
+  editorDrawRows(&ab);
+
+  abAppend(&ab,"\x1b[H",3);
+  abAppend(&ab, "\x1b[?25h",6); //cursor plz come back
+  write(STDOUT_FILENO,ab.b,ab.len);
+  abFree(&ab);
 }
 
 /*** input ***/
